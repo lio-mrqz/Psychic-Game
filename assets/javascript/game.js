7c59efd764939d58df9e64440eece05174d5fa94
Lio <lionell.marquez@gmail.com>
2019-04-02 21:24:28 -0400

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,8 @@
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var wins = 0;
 var losses = 0;
-
+var guessesLeft = 5;
+var psychicGuess = psychicGuess = letters[Math.floor(Math.random() * letters.length)];
 var userInputText = document.getElementById("userInput");
 var winsText = document.getElementById("winsText");
 var lossesText = document.getElementById("lossesText");
@@ -12,16 +13,20 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     // var check = return (event.charCode > 64 && 
         // event.charCode < 91) || (event.charCode > 96 && event.charCode < 123);
-    var psychicGuess = letters[Math.floor(Math.random() * letters.length)];
+    
 
     if ((userGuess === "a") ||(userGuess === "b") ||(userGuess === "c") ||(userGuess === "d") ||(userGuess === "e") ||(userGuess === "f") ||(userGuess === "g") ||(userGuess === "h") ||(userGuess === "i") ||(userGuess === "j") ||(userGuess === "k") ||(userGuess === "l") ||(userGuess === "m") ||(userGuess === "n") ||(userGuess === "o") ||(userGuess === "p") ||(userGuess === "q") ||(userGuess === "r") ||(userGuess === "s") ||(userGuess === "t") ||(userGuess === "u") ||(userGuess === "v") ||(userGuess === "w") ||(userGuess === "x") ||(userGuess === "y") ||(userGuess === "z")) {
         if (userGuess === psychicGuess) { wins++; }
-        else { losses++; }
+        guessesLeft--;
+        if (guessesLeft == 0){ losses++ }
 
         userInputText.textContent = "You chose: " + userGuess;
         winsText.textContent = "Wins: " + wins; 
         lossesText.textContent = "losses: " + losses; 
         
     }
-   
+   function gameReset() {
+       guessesLeft = 5;
+        psychicGuess = 
+   }
 }
